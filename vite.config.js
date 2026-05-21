f9ee5b084a2d2585e6c452783297a2c4a56c14ae
kerllySamantha <genefebre21@gmail.com>
2026-05-21 20:55:28 +0200

    añadir a vite la hoja de estilos de auth

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 
+            input: [
+                'resources/css/app.css', 
                 'resources/css/admin/admin-styles.css', 'resources/js/app.js',
-                 'resources/css/empresa/empresa.css', 'resources/images/logo-sinfondo.png'],
+                 'resources/css/empresa/empresa.css', 'resources/images/logo-sinfondo.png', 'resources/css/auth/auth.css'],
             refresh: true,
         }),
     ],
